cc05fddda2c76756f4039ac3554c6d2d3801d4ac
1091214370 <1091214370@qq.com>
2017-11-26 23:52:04 +0800

    增加页面，完善响应式功能

---
+++ js/findOut.js
@@ -0,0 +1,42 @@
+let vm = new Vue({
+    el: "#inp",
+    data: {
+        key: "",
+        itemCount: "",
+        showResult: false,
+        items: [],
+        tip: ""
+    },
+    methods: {
+        clickFun: function() {
+            let xml = new XMLHttpRequest();
+            xml.open('get', "https://api.imjad.cn/cloudmusic/?type=search&s=" + this.key + "&br=128000&limit=10", true);
+            xml.send();
+            xml.onreadystatechange = () => {
+                if (xml.readyState === 4 && xml.status === 200) {
+                    var res = JSON.parse(xml.responseText);
+                    if (res.result) {
+                        this.showResult = true;
+                        this.items = res.result.songs;
+                        this.itemCount = res.result.songCount;
+                    } else {
+                        this.showResult = false;
+                        alert("未找到资源！");
+                    }
+                }
+            }
+        },
+        sing: function(e) {
+            let id = e.target.id;
+            let xml = new XMLHttpRequest();
+            xml.open('get', "https://api.imjad.cn/cloudmusic/?type=song&id=" + id + "&br=128000", true);
+            xml.send();
+            xml.onreadystatechange = () => {
+                if (xml.readyState === 4 && xml.status === 200) {
+                    var res = JSON.parse(xml.responseText);
+                    document.getElementById("audio").src = res.data[0].url;
+                }
+            };
+        }
+    }
+})
